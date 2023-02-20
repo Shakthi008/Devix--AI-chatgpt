97b845540721adaf00bcb0c2a9a80540bfac8844
shakthi priyan <94235615+Shakthi008@users.noreply.github.com>
2023-02-20 21:48:40 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handlesubmit = async (e) => {
 
   loader(messageDiv)
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://devix.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
